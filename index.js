3ee40f3cd14f35a754386ae031edaacd884450f9
Raj Maharjan <rmaharjan.rm90@gmail.com>
2022-06-11 19:52:20 +0545

    localhost remove

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ const app = express();
 app.set('view options', { layout: false });
 app.use(express.static(__dirname + '/static'));
 
-const hostname = '127.0.0.1';
-
 // app.use(express.static(__dirname, '/static'));
 
 app.use('/', renderStatic);
 
-app.listen(process.env.port || 3000);
-
-console.log('Running at port:300', hostname, (err, done) => {
+app.listen(process.env.port || 3000, (err, done) => {
 	if (err) {
 		console.log('error running site');
 	} else {
